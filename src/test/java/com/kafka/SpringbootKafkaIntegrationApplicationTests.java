746620cec7c8d160950397520cf50d26f2d51531
aniketkumar5256 <aniketkumar5256@gmail.com>
2025-11-23 15:28:28 +0530

    kafka-integration

---
+++ src/test/java/com/kafka/SpringbootKafkaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
